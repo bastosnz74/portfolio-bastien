11072a788fe03cd4957c79dde459ab38d8f7ae6a
bastosnz74 <80914441+bastosnz74@users.noreply.github.com>
2023-10-22 12:08:13 +0200

    Update Header.jsx

--- front_end/src/container/Header/Header.jsx
+++ front_end/src/container/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Bonjour, je suis</p>
-            <h1 className="head-text">Bastien.</h1>
+            <h1 className="head-text">Bastien MAROZZA.</h1>
           </div>
         </div>
 
