7d76f83d8f7d8d786d990b17623349f51d645314
bastosnz74 <80914441+bastosnz74@users.noreply.github.com>
2024-02-08 16:40:26 +0100

    Update Footer.jsx

--- front_end/src/container/Footer/Footer.jsx
+++ front_end/src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:basti74600@hotmail.fr" className="p-text">basti74600@hotmail.fr</a>
+          <a href="mailto:basti74600@hotmail.fr" className="p-text">marozzabastien@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
